b08821f988fac29338b0d62455edf3cf0a8903cf
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2025-03-24 04:08:38 +0330

    add `ms` to morgan logger

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import morgan from 'morgan';
 dotenv.config();
 const app = express();
 
-app.use(morgan(':date[web] | :url <:method, :status> | :response-time[3]'));
+app.use(morgan(':date[web] | :url <:method, :status> | :response-time[3]ms'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
