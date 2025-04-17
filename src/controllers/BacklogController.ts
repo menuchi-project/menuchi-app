cd2556b2045662ef213cdbd4c15f39a28ea32d7d
Mohammad Hosseini <mimhe1381@gmail.com>
2025-04-17 19:25:14 +0330

    Update Backlog Controller

--- src/controllers/BacklogController.ts
+++ src/controllers/BacklogController.ts
@@ -70,24 +70,78 @@ export class BacklogController extends BaseController {
     return null;
   }
 
+  @Response<ForbiddenError>(403, 'Access Denied. You are not authorized to perform this action.')
+  @Response<UnauthorizedError>(401, 'Unauthorized user.')
   @Response<MenuchiError>(400, 'All item IDs must be in the request.')
   @SuccessResponse(204, 'Item orders in the category updated successfully.')
+  @Security('', [RolesEnum.RestaurantOwner])
   @Patch('/{backlogId}/reorder-items/in-category')
-  public async reorderItemsInCategory(@Path() backlogId: UUID, @Body() body: UUID[]): Promise<number> {
+  public async reorderItemsInCategory(
+    @Path() backlogId: UUID,
+    @Body() body: UUID[],
+    @Request() req: express.Request
+  ): Promise<number> {
+    this.checkPermission(req.session.user, PermissionScope.Backlog, backlogId);
     return BacklogService.reorderItemsInCategory(backlogId, body);
   }
 
+  @Response<ForbiddenError>(403, 'Access Denied. You are not authorized to perform this action.')
+  @Response<UnauthorizedError>(401, 'Unauthorized user.')
   @Response<MenuchiError>(400, 'All item IDs must be in the request.')
   @SuccessResponse(204, 'Item orders in the list updated successfully.')
+  @Security('', [RolesEnum.RestaurantOwner])
   @Patch('/{backlogId}/reorder-items/in-list')
-  public async reorderItemsInList(@Path() backlogId: UUID, @Body() body: UUID[]): Promise<number> {
+  public async reorderItemsInList(
+    @Path() backlogId: UUID,
+    @Body() body: UUID[],
+    @Request() req: express.Request
+  ): Promise<number> {
+    this.checkPermission(req.session.user, PermissionScope.Backlog, backlogId);
     return BacklogService.reorderItemsInList(backlogId, body);
   }
 
+  @Response<ForbiddenError>(403, 'Access Denied. You are not authorized to perform this action.')
+  @Response<UnauthorizedError>(401, 'Unauthorized user.')
   @SuccessResponse(204, 'Items deleted successfully. It doesn\'t retrieve anything.')
+  @Security('', [RolesEnum.RestaurantOwner])
   @Delete('/{backlogId}/items')
-  public async deleteItems(@Path() backlogId: UUID, @Body() body: UUID[]): Promise<null> {
+  public async deleteItems(
+    @Path() backlogId: UUID,
+    @Body() body: UUID[],
+    @Request() req: express.Request
+  ): Promise<null> {
+    this.checkPermission(req.session.user, PermissionScope.Backlog, backlogId);
     await BacklogService.deleteItems(backlogId, body);
+    return null;
+  }
+
+  @Response<ForbiddenError>(403, 'Access Denied. You are not authorized to perform this action.')
+  @Response<UnauthorizedError>(401, 'Unauthorized user.')
+  @Response<MenuchiError>(400, 'All category IDs must be in the request.')
+  @Security('', [RolesEnum.RestaurantOwner])
+  @SuccessResponse(204, 'Category orders in the backlog updated successfully.')
+  @Patch('/{backlogId}/reorder-categories')
+  public async reorderCategoriesInBacklog(
+    @Path() backlogId: UUID,
+    @Body() body: UUID[],
+    @Request() req: express.Request
+  ): Promise<number> {
+    this.checkPermission(req.session.user, PermissionScope.Backlog, backlogId);
+    return BacklogService.reorderCategoriesInBacklog(backlogId, body);
+  }
+
+  @Response<ForbiddenError>(403, 'Access Denied. You are not authorized to perform this action.')
+  @Response<UnauthorizedError>(401, 'Unauthorized user.')
+  @SuccessResponse(204, 'Category and its items deleted successfully.  It doesn\'t retrieve anything.')
+  @Security('', [RolesEnum.RestaurantOwner])
+  @Delete('/{backlogId}/categories/{categoryId}')
+  public async deleteCategory(
+    @Path() backlogId: UUID,
+    @Path() categoryId: UUID,
+    @Request() req: express.Request
+  ): Promise<null> {
+    this.checkPermission(req.session.user, PermissionScope.Backlog, backlogId);
+    await BacklogService.deleteCategory(backlogId, categoryId);
     return null;
   }
 }
--- src/services/BacklogService.ts
+++ src/services/BacklogService.ts
@@ -263,6 +263,18 @@ class BacklogService {
     if (!isValidQuery) throw new MenuchiError('All item IDs must be in the request.', 400);
   }
 
+  async reorderCategoriesInBacklog(backlogId: UUID, categoriesId: UUID[]) {
+    await this.isValidCategoriesId(backlogId, categoriesId);
+    return this.prisma.$executeRaw`
+      UPDATE "categories"
+      SET "position_in_backlog" = CASE "id"
+        ${Prisma.join(categoriesId.map((categoryId, index) => Prisma.sql`WHEN ${categoryId}::uuid THEN ${index + 1}`), ' ')}
+      ELSE "position_in_backlog"
+      END
+      WHERE "id" IN (${Prisma.join(categoriesId.map(id => Prisma.sql`${id}::uuid`))})
+    `;
+  }
+
   async deleteCategory(backlogId: UUID, categoryId: UUID) {
     return this.prisma.$transaction(async (tx) => {
       await tx.category.update({
@@ -291,6 +303,19 @@ class BacklogService {
         }
       });
     });
+  }
+
+  private async isValidCategoriesId(backlogId: UUID, categoriesId: UUID[]): Promise<void | never> {
+    const categories = await this.prisma.category.findMany({
+      where: {
+        deletedAt: null,
+        backlogId
+      }
+    });
+
+    const isValidQuery = (categories.length === categoriesId.length) &&
+            categories.every(category => categoriesId.some(categoryId => categoryId === category.id ));
+    if (!isValidQuery) throw new MenuchiError('All category IDs must be in the request.', 400);
   }
 }
 
