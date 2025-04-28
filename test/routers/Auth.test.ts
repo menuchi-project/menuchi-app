1398523f88eb4013f9e5ed80a61c3857dabecf82
Mohammad Hosseini <mimhe1381@gmail.com>
2025-04-28 22:31:22 +0330

    Test setups and Write first test

---
+++ test/routers/Auth.test.ts
@@ -0,0 +1,25 @@
+import { describe } from 'node:test';
+import createServer from '../../src/server';
+import { expect, test } from 'vitest';
+import supertest from 'supertest';
+import { createUser } from '../factories';
+import prisma from '../../libs/__mocks__/prisma';
+
+const request = supertest(createServer());
+
+describe('POST /auth/res-signup', () => {
+  test('should return the new user with 201 status code.', async () => {
+  const userObject = createUser();
+  prisma.user.create.mockResolvedValue(userObject);
+
+  const res = await request
+    .post('/auth/res-signup')
+    .send(userObject);
+
+  expect(res.status).toBe(201);
+  expect(res.body.phoneNumber).toMatch(userObject.phoneNumber);
+  expect(res.body.password).toMatch(userObject.password);
+  expect(res.body.email).toMatch(userObject.email);
+  expect(res.body.username).toMatch(userObject.username);
+  });
+});
